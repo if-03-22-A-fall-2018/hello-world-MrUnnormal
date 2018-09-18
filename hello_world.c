8193fdbc86b157dfc3b9b5fd90ebefa275c13154
Jan Kaufmann <jan.kaufmann@liwest.at>
2018-09-18 09:44:27 +0200

    first version

---
+++ hello_world.c
@@ -0,0 +1,19 @@
+
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2AHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 0
+ * Title:		 hello_World
+ * Author:			Jan Kaufmann
+ * ----------------------------------------------------------
+ * Description:
+ * Prints out my full name.
+ * ----------------------------------------------------------
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  printf(" Hello Jan Kaufmann \n");
+  return 0;
+}
